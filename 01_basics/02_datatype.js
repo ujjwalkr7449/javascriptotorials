c3b8c7574630a8f9788b07f815fb8185e499a9eb
UJJWAL KUMAR <111566155+ujjwalkr7449@users.noreply.github.com>
2024-10-12 20:24:44 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict";// treat all js code as newer version
+//alert(3+3) //we using nodejs , not brower
+console.log(3+3)//code readibility should be high
+console.log("hitesh") 
+/*
+number=>2**53
+bigint
+string=>""/''
+boolean=>true/false
+null=>standalone value 
+undifiened
+symbol=> it sue in react and unique
+object */
+console.log(typeof undefined)//undefiend
+console.log(typeof null);//object
